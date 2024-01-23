5a8881f984b766f2cd94882dd62903a342512081
veronbungag <rveron.bungag@gmail.com>
2024-01-23 22:54:50 +0800

    adding lines for more organize look

--- removing_n_characters_from_string.py
+++ removing_n_characters_from_string.py
@@ -1,7 +1,9 @@
 #defining a function for removing characters
 def remove_characters(word, n):
-# return a slice of the string from n to the end
- return word[n:]
+    #print original string
+    print('Original string:', word)
+    #return a slice of the string from n to the end
+    return word[n:]
 
 # test the function and print
 print(remove_characters("Footlong", 4)) 
